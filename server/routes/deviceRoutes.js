15d2891aa197fc7d28b8195fba2450dd5b1577c4
Lilights <100maz100@gmail.com>
2026-01-04 13:51:10 +0200

    backend changes from shani (except AI module)

--- server/routes/deviceRoutes.js
+++ server/routes/deviceRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const  {createDevice , deleteDevice ,validateDeviceToken} = require('../controllers/deviceController')
+const  { getDevices, createDevice , deleteDevice ,validateDeviceToken} = require('../controllers/deviceController')
+const requireAuth = require('../middleware/requireAuth')
 
+router.get('/validate/:token', validateDeviceToken)
+
+router.use(requireAuth)
+
+//Require auth for all device routes
+router.get('/', getDevices)
 router.post('/create', createDevice)
 router.delete('/:deviceId', deleteDevice)
-router.get('/validate/:token', validateDeviceToken)
+
 
 module.exports = router
